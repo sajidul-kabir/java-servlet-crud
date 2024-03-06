752d1cc7c941f529a88b367c916282ebdd7d16f9
sajidul-kabir <sajidulkabiradib@gmail.com>
2024-03-06 15:45:15 +0600

    xml congigurataion for servlets add

--- src/servlet/OrderServlet.java
+++ src/servlet/OrderServlet.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet({"/orders/*"})
+//@WebServlet({"/orders/*"})
 public class OrderServlet extends HttpServlet {
     private OrderDao orderDao;
 
--- src/servlet/ProductServlet.java
+++ src/servlet/ProductServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet({"/products/*"})
+//@WebServlet({"/products/*"})
 public class ProductServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private ProductDao productDAO;
